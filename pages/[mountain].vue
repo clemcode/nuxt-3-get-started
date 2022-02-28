bc778c46667d3f7e5bf29b9436f4e4a748207f2e
clemcode <clement.o2p@gmail.com>
2022-02-28 14:54:31 +0100

    4 binaries uploaded

--- pages/[mountain].vue
+++ pages/[mountain].vue
@@ -4,7 +4,7 @@
       {{ $route.params.mountain }}
     </h1>
     <img
-      :src="`/images/${$route.params.mountain}.jpg`"
+      :src="`/images/${$route.params.mountain}.png`"
       :alt="$route.params.mountain"
     />
   </section>
